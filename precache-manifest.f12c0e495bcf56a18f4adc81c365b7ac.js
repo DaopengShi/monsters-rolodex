322d2e0632b7d5858c61c08b0b6646b48edddbbc
DaopengShi <holmessherlock734@gmail.com>
2019-08-01 23:30:43 +0800

    Updates

---
+++ precache-manifest.f12c0e495bcf56a18f4adc81c365b7ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72091d0ee3db129a6372c2f05e1e66d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "83ecb04c2ba398cc89f2",
+    "url": "/monsters-rolodex/static/css/main.47b083b7.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "83ecb04c2ba398cc89f2",
+    "url": "/monsters-rolodex/static/js/main.28db65f9.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
